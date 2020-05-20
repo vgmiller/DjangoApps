f82ad62e8d280d40bce3205f968bfad855b3a478
vgmiller <vanessa.g.miller@gmail.com>
2020-05-20 14:20:31 -0700

    Some config and acknowledgement changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'naga',
 ]
 
+MIDDLEWARE_CLASSES = [
+    'sslify.middleware.SSLifyMiddleware',
+        ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
