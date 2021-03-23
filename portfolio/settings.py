77c3ec9894dae2ad6459d6ee2384bca85fa1a97b
vgmiller <vanessa.g.miller@gmail.com>
2021-03-23 16:50:31 -0700

    Tweaks and permissions

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xs6xml@@ru5nnj@))n!mm+17p_6yoqbs(t6-4^l4-09riy%tko'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
         'vgmraspberrypi',
