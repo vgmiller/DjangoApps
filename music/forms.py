8d1784461c32266a57e8491bc1e8f180c91bbe34
vgmiller <vanessa.g.miller@gmail.com>
2022-01-14 18:32:55 -0500

    Shine up css and content on music site

--- music/forms.py
+++ music/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.forms import TextInput, EmailInput
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    name = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'formField', 'placeholder': 'Name'}), required=True)
+    from_email = forms.EmailField(label="", widget=forms.EmailInput(attrs={'class': 'formField', 'placeholder': 'Email'}), required=True)
+    subject = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'formField', 'placeholder': 'Subject'}), required=True)
+    message = forms.CharField(label="", widget=forms.Textarea(attrs={'class': 'formField', 'placeholder': 'Message'}), required=True)
