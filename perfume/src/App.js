85ba1ce44cc38e4bd6205b9869482cd5dc62bc4e
vgmiller <vanessa.g.miller@gmail.com>
2023-11-29 14:59:33 -0500

    Update perfume styling

--- perfume/src/App.js
+++ perfume/src/App.js
@@ -96,7 +96,7 @@ class App extends Component {
     render() {
       return (
         <main className="content">
-        <h1 className="text-uppercase text-center my-4" style={{color:"MediumVioletRed", background:"LightCyan"}}>Eau du Jour</h1>
+        <h1 className="text-uppercase text-center my-4" style={{color:"MediumVioletRed", background:"LavenderBlush"}}>Eau du Jour</h1>
         <div className="row">
           <div className="col-md-6 col-sm-10 mx-auto p-0">
             <div className="card p-3">
