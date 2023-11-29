136b5f733432ae7b6c24ccdedf896583a85f355a
vgmiller <vanessa.g.miller@gmail.com>
2023-11-29 17:48:03 -0500

    re-version todo webpack

--- todo/webpack.config.js
+++ todo/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new BundleTracker({
       path: __dirname,
-      filename: './webpack-stats.json',
+      filename: 'webpack-stats.json',
     }),
   ],
   module: {
