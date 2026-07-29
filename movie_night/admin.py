cc872acad8ea4d2a945c3eb6f276ec9e96015753
vgmiller <vanessa.g.miller@gmail.com>
2026-07-28 22:00:09 -0400

    admin cleanup

--- movie_night/admin.py
+++ movie_night/admin.py
@@ -19,21 +19,32 @@ class ProfileAdmin(admin.ModelAdmin):
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ("name", "invite_code", "created_by", "created_at")
+    search_fields = ("name", "invite_code")
 
 
 @admin.register(GroupMember)
 class GroupMemberAdmin(admin.ModelAdmin):
     list_display = ("group", "user", "role", "joined_at")
+    list_filter = ("role",)
+    autocomplete_fields = ("group",)
+    raw_id_fields = ("user",)
 
 
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ("title", "group", "submitted_by", "type", "created_at")
+    list_filter = ("type",)
+    search_fields = ("title", "description")
+    autocomplete_fields = ("group",)
+    raw_id_fields = ("submitted_by",)
 
 
 @admin.register(ActivityInterest)
 class ActivityInterestAdmin(admin.ModelAdmin):
     list_display = ("activity", "user", "level", "updated_at")
+    list_filter = ("level",)
+    autocomplete_fields = ("activity",)
+    raw_id_fields = ("user",)
 
 
 @admin.register(AvailabilitySlot)
